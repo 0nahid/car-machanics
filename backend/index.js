d5997652f04a4a59452ed7459a9c69330a4df84e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-06 16:35:38 +0600

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5500;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@genius.r5hwg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  (await client.connect())
+    ? console.log("connected to the database")
+    : await client.close();
+
+  const collection = client.db("Hotel").collection("services");
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
