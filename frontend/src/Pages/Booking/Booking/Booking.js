b6074cd669d5cb23b6dd83c2dfab09b43db5c926
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-07 01:03:43 +0600

    create and read fixed

--- frontend/src/Pages/Booking/Booking/Booking.js
+++ frontend/src/Pages/Booking/Booking/Booking.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5500/api/services/${serviceId}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [serviceId])
     return (
         <div>
             <h2>this is booking: {serviceId}</h2>
+            <h1>{service?.name}</h1>
+            <p>{service?.price}</p>
+            <p>{service?.description}</p> 
+            <img src={service?.img} alt="" />
         </div>
     );
 };
--- frontend/src/Pages/Home/Service/Service.js
+++ frontend/src/Pages/Home/Service/Service.js
@@ -2,13 +2,12 @@ import { Link } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({ service }) => {
-    const { _id, name, price, description, img } = service;
+    const { _id, name, price, img } = service;
     return (
-        <div className="service pb-3">
+        <div className="service pb-3 ">
             <img src={img} alt="" />
             <h3>{name}</h3>
-            <h5>Price: {price}</h5>
-            <p className="px-3">{description}</p>
+            <h5>Price: {price} $</h5>
             <Link to={`/booking/${_id}`}>
                 <button className="btn btn-warning">Book {name.toLowerCase()}</button>
             </Link>
